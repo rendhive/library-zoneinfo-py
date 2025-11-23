2bcabe799c867617c050ca2c59d8b981fdcff917
rendhivecode <rendhivecode@gmail.com>
2025-11-24 06:10:14 +0700

    Add: Mengimpor Modul zoneinfo

---
+++ 1. Mengimpor Modul zoneinfo.py
@@ -0,0 +1,5 @@
+from zoneinfo import ZoneInfo
+
+print("Modul zoneinfo berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk menangani zona waktu.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari modul zoneinfo.
