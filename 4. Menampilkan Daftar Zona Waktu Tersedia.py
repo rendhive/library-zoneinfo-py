159636fd37a6003da18339b531278004e37082f6
rendhivecode <rendhivecode@gmail.com>
2025-11-24 06:18:04 +0700

    Add: 4. Menampilkan Daftar Zona Waktu Tersedia.py

---
+++ 4. Menampilkan Daftar Zona Waktu Tersedia.py
@@ -0,0 +1,7 @@
+from zoneinfo import available_timezones
+
+print("Zona waktu yang tersedia:")
+for timezone in available_timezones():
+    print(timezone)
+# Fungsi: Menampilkan semua zona waktu yang tersedia di Python.
+# Kondisi: Ketika Anda perlu mengetahui zona waktu yang bisa digunakan.
