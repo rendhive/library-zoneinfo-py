068c1eb5d043977622f084ea26fdbf7c38ba3190
rendhivecode <rendhivecode@gmail.com>
2025-11-24 12:23:05 +0700

    [DOC] Sample Library Python: 14. Mendeskripsikan Zona Waktu.py

---
+++ 14. Mendeskripsikan Zona Waktu.py
@@ -0,0 +1,6 @@
+from zoneinfo import ZoneInfo
+
+zone = ZoneInfo('Asia/Kolkata')
+print("Nama zona waktu:", zone.zone)
+# Fungsi: Mendapatkan nama zona waktu.
+# Kondisi: Ketika Anda ingin menampilkan nama zona waktu untuk referensi.
